b25232e78a2b30494129d20b8cc83c61da384f99
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-23 16:36:21 +0200

    update

---
+++ app/(auth)/signup/SignUpForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+import { signUpSchema, SignUpValues } from "@/lib/validation";
+import React, { useState, useTransition } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signUp } from "./actions";
+import { PasswordInput } from "@/components/PasswordInput";
+
+const SignUpForm = () => {
+  const [error, setError] = useState<string>();
+
+  const [isPending, startTransition] = useTransition();
+
+  const form = useForm<SignUpValues>({
+    resolver: zodResolver(signUpSchema),
+    defaultValues: {
+      email: "",
+      username: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(values: SignUpValues) {
+    setError(undefined);
+    startTransition(async () => {
+      const { error } = await signUp(values);
+      if (error) setError(error);
+    });
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
+        {error && <p className="text-center text-destructive">{error}</p>}
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="Username" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Email" type="email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <PasswordInput placeholder="Password" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit" className="w-full">
+          Create Account
+        </Button>
+      </form>
+    </Form>
+  );
+};
+
+export default SignUpForm;
--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -1,6 +1,8 @@
 import { Metadata } from "next";
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import SignUpForm from "./SignUpForm";
 
 export const metadata: Metadata = {
   title: "Sign Up",
@@ -10,13 +12,27 @@ const SignUpPage = () => {
   return (
     <main className="flex h-screen items-center justify-center p-5">
       <div className="flex h-full max-h-[40rem] w-full max-w-[64rem] overflow-hidden rounded-2xl bg-card shadow-2xl">
-        <div className="md:w-1/2">Login Form</div>
+        <div className="w-full space-y-10 overflow-y-auto p-10 md:w-1/2">
+          <div className="space-y-1 text-center">
+            <h1 className="text-3xl font-bold">Sign up to Clodron Social</h1>
+            <p className="text-muted-foreground">
+              A place where even <span className="font-bold italic">you</span>{" "}
+              can find a friend.
+            </p>
+          </div>
+          <div className="space-y-5">
+            <SignUpForm />
+            <Link href={"/login"} className="block text-center hover:underline">
+              Already have an account? Login.
+            </Link>
+          </div>
+        </div>
         <Image
           src={"/signup.png"}
           alt="SignUp"
           width={1000}
           height={1000}
-          className="hidden w-1/2 md:block object-cover"
+          className="hidden w-1/2 object-cover md:block"
         />
       </div>
     </main>
