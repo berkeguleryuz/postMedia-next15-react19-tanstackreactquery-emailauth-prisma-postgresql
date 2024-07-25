f9a202841e5424d5df4025f0790064eda7d292a0
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-25 14:04:29 +0200

    react query update

--- components/posts/Post.tsx
+++ components/posts/Post.tsx
@@ -1,34 +1,46 @@
+"use client";
 import { PostData } from "@/lib/types";
 import Link from "next/link";
 import React from "react";
 import UserAvatar from "../UserAvatar";
 import { formatRelativeDate } from "@/lib/utils";
+import { useSession } from "@/app/(main)/SessionProvider";
+import PostMoreButton from "./PostMoreButton";
 
 interface PostProps {
   post: PostData;
 }
 
 const Post = ({ post }: PostProps) => {
+  const { user } = useSession();
   return (
-    <article className="space-y-3 rounded-2xl bg-foreground p-5 text-input shadow-sm">
-      <div className="flex flex-wrap gap-3">
-        <Link href={`/users/${post.user.username}`}>
-          <UserAvatar avatarUrl={post.user.avatarUrl} />
-        </Link>
-        <div>
-          <Link
-            href={`/users/${post.user.username}`}
-            className="block font-medium hover:underline"
-          >
-            {post.user.displayName}
+    <article className="group/post space-y-3 rounded-2xl bg-foreground p-5 text-input shadow-sm">
+      <div className="flex justify-between gap-3">
+        <div className="flex flex-wrap gap-3">
+          <Link href={`/users/${post.user.username}`}>
+            <UserAvatar avatarUrl={post.user.avatarUrl} />
           </Link>
-          <Link
-            href={`/posts/${post.id}`}
-            className="block text-sm text-muted-foreground hover:underline"
-          >
-            {formatRelativeDate(post.createdAt)}
-          </Link>
+          <div>
+            <Link
+              href={`/users/${post.user.username}`}
+              className="block font-medium hover:underline"
+            >
+              {post.user.displayName}
+            </Link>
+            <Link
+              href={`/posts/${post.id}`}
+              className="block text-sm text-muted-foreground hover:underline"
+            >
+              {formatRelativeDate(post.createdAt)}
+            </Link>
+          </div>
         </div>
+        {post.user?.id === user.id && (
+          <PostMoreButton
+            post={post}
+            className="opacity-0 transition-opacity group-hover/post:opacity-100 text-foreground/50"
+          />
+        )}
       </div>
       <div className="whitespace-pre-line break-words">{post.content}</div>
     </article>
---
+++ components/posts/PostMoreButton.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { PostData } from "@/lib/types";
+import React, { useState } from "react";
+import DeletePostDialog from "./DeletePostDialog";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "../ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { MoreHorizontal, Trash2 } from "lucide-react";
+
+interface PostMoreButtonProps {
+  post: PostData;
+  className?: string;
+}
+const PostMoreButton = ({ post, className }: PostMoreButtonProps) => {
+  const [showDeleteDialog, setShowDeleteDialog] = useState(false);
+  return (
+    <div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button size={"icon"} variant={"ghost"} className={className}>
+            <MoreHorizontal className="size-5 text-muted-foreground" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent>
+          <DropdownMenuItem onClick={() => setShowDeleteDialog(true)}>
+            <span className="flex items-center gap-3 text-destructive">
+              <Trash2 className="size-4" />
+              Delete
+            </span>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      <DeletePostDialog
+        post={post}
+        open={showDeleteDialog}
+        onClose={() => setShowDeleteDialog(false)}
+      />
+    </div>
+  );
+};
+
+export default PostMoreButton;
