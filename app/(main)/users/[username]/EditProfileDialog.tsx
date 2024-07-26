e98bfe8b397024b5dcaa7abd2f1176fb29d999d0
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-26 15:36:21 +0200

    avatar resize and actions

--- app/(main)/users/[username]/EditProfileDialog.tsx
+++ app/(main)/users/[username]/EditProfileDialog.tsx
@@ -11,7 +11,7 @@ import {
   UpdateUserProfileValues,
 } from "@/lib/validation";
 import { zodResolver } from "@hookform/resolvers/zod";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { useForm } from "react-hook-form";
 import { useUpdateProfileMutation } from "./mutations";
 import {
@@ -25,6 +25,13 @@ import {
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
 import LoadingButton from "@/components/LoadingButton";
+import Image, { StaticImageData } from "next/image";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import avatarPlaceHolder from "@/public/avatar-placeholder.png";
+import { Camera } from "lucide-react";
+import CropImageDialog from "@/components/CropImageDialog";
+import Resizer from "react-image-file-resizer";
+
 interface EditProfileDialogProps {
   user: UserData;
   open: boolean;
@@ -46,13 +53,21 @@ const EditProfileDialog = ({
 
   const mutation = useUpdateProfileMutation();
 
+  const [croppedAvatar, setCroppedAvatar] = useState<Blob | null>(null);
+
   async function onSubmit(values: UpdateUserProfileValues) {
+    const newAvatarFile = croppedAvatar
+      ? new File([croppedAvatar], `avatar_${user.id}.webp`)
+      : undefined;
+
     mutation.mutate(
       {
         values,
+        avatar: newAvatarFile,
       },
       {
         onSuccess: () => {
+          setCroppedAvatar(null);
           onOpenChange(false);
         },
       },
@@ -64,6 +79,17 @@ const EditProfileDialog = ({
         <DialogHeader>
           <DialogTitle>Edit profile</DialogTitle>
         </DialogHeader>
+        <div className="space-y-1.5">
+          <Label>Avatar</Label>
+          <AvatarInput
+            src={
+              croppedAvatar
+                ? URL.createObjectURL(croppedAvatar)
+                : user.avatarUrl || avatarPlaceHolder
+            }
+            onImageCropped={setCroppedAvatar}
+          />
+        </div>
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-3">
             <FormField
@@ -111,3 +137,70 @@ const EditProfileDialog = ({
 };
 
 export default EditProfileDialog;
+
+interface AvatarInputProps {
+  src: string | StaticImageData;
+  onImageCropped: (blob: Blob | null) => void;
+}
+
+function AvatarInput({ src, onImageCropped }: AvatarInputProps) {
+  const [imageToCrop, setImageToCrop] = useState<File>();
+
+  const fileInputRef = useRef<HTMLInputElement>(null);
+
+  function onImageSelected(image: File | undefined) {
+    if (!image) return;
+
+    Resizer.imageFileResizer(
+      image,
+      1024,
+      1024,
+      "WEBP",
+      100,
+      0,
+      (uri) => setImageToCrop(uri as File),
+      "file",
+    );
+  }
+
+  return (
+    <div className="">
+      <input
+        type="file"
+        accept="image/*"
+        onChange={(e) => onImageSelected(e.target.files?.[0])}
+        ref={fileInputRef}
+        className="sr-only hidden"
+      />
+      <button
+        className="group relative block"
+        onClick={() => fileInputRef.current?.click()}
+        type="button"
+      >
+        <Image
+          src={src}
+          alt="Avatar preview"
+          width={150}
+          height={150}
+          className="size-32 flex-none rounded-full object-cover"
+        />
+        <span className="absolute inset-0 m-auto flex size-12 items-center justify-center rounded-full bg-black bg-opacity-30 text-white transition-colors duration-200 group-hover:bg-opacity-25">
+          <Camera size={"24"} />
+        </span>
+      </button>
+      {imageToCrop && (
+        <CropImageDialog
+          src={URL.createObjectURL(imageToCrop)}
+          cropAspectRatio={1}
+          onCropped={onImageCropped}
+          onClose={() => {
+            setImageToCrop(undefined);
+            if (fileInputRef.current) {
+              fileInputRef.current.value = "";
+            }
+          }}
+        />
+      )}
+    </div>
+  );
+}
---
+++ components/CropImageDialog.tsx
@@ -0,0 +1,58 @@
+import React, { useRef } from "react";
+import { Cropper, ReactCropperElement } from "react-cropper";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import "cropperjs/dist/cropper.css";
+interface CropImageDialogProps {
+  src: string;
+  cropAspectRatio: number;
+  onCropped: (blob: Blob | null) => void;
+  onClose: () => void;
+}
+const CropImageDialog = ({
+  src,
+  cropAspectRatio,
+  onCropped,
+  onClose,
+}: CropImageDialogProps) => {
+  const cropperRef = useRef<ReactCropperElement>(null);
+
+  function crop() {
+    const cropper = cropperRef.current?.cropper;
+    if (!cropper) return;
+
+    cropper.getCroppedCanvas().toBlob((blob) => onCropped(blob), "image/webp");
+    onClose();
+  }
+  return (
+    <Dialog open onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>Crop image</DialogTitle>
+        </DialogHeader>
+        <Cropper
+          src={src}
+          aspectRatio={cropAspectRatio}
+          guides={false}
+          zoomable={false}
+          ref={cropperRef}
+          className="mx-auto size-fit"
+        />
+        <DialogFooter>
+          <Button variant={"secondary"} onClick={onClose}>
+            Cancel
+          </Button>
+          <Button onClick={crop}>Crop</Button>
+        </DialogFooter>
+      </DialogContent>
+    </Dialog>
+  );
+};
+
+export default CropImageDialog;
