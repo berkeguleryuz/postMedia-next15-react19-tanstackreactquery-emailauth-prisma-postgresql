03f8b41984235cc27aef601dd640243c65e5c52f
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-07-26 15:51:28 +0200

    attachments video image etc

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,7 +1,7 @@
 import { validateRequest } from "@/auth";
 import prisma from "@/lib/prisma";
 import { createUploadthing, FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 
 const f = createUploadthing();
 
@@ -17,6 +17,15 @@ export const fileRouter = {
       return { user };
     })
     .onUploadComplete(async ({ metadata, file }) => {
+      const oldAvatarUrl = metadata.user.avatarUrl;
+
+      if (oldAvatarUrl) {
+        const key = oldAvatarUrl.split(
+          `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
+        )[1];
+
+        await new UTApi().deleteFiles(key);
+      }
       const newAvatarUrl = file.url.replace(
         "/f/",
         `/a/${process.env.NEXT_PUBLIC_UPLOADTHING_APP_ID}/`,
