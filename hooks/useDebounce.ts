91feafdbde5f530439f8059198f58b019121d582
Berke Güleryüz <berkeguleryuz@gmail.com>
2024-08-04 13:58:29 +0200

    deployment

--- hooks/useDebounce.ts
+++ hooks/useDebounce.ts
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 
+//
+
 export default function useDebounce<T>(value: T, delay: number = 250): T {
   const [debouncedValue, setDebouncedValue] = useState<T>(value);
 
